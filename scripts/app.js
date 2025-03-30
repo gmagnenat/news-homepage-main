0ce30bd280e5f8fa5d920c62dc78df4566fddb79
G. Mag <g.magnenat@gmail.com>
2025-03-30 15:16:42 +0200

    Add logic for the mobile side navigation

---
+++ scripts/app.js
@@ -0,0 +1,70 @@
+const mobileNavigation = document.querySelector(".mobile-navigation");
+const openNavigationBtn = document.querySelector(".open-navigation");
+const closeNavigationBtn = document.querySelector(".close-navigation");
+const body = document.querySelector("body");
+
+function setupEventListeners() {
+  openNavigationBtn.addEventListener("click", openMobileNavigation);
+  closeNavigationBtn.addEventListener("click", closeMobileNavigation);
+  mobileNavigation.addEventListener("keydown", handleKeyDown);
+  mobileNavigation.addEventListener("click", handleDialogClick);
+}
+
+function handleDialogClick(e) {
+  const dialogDimensions = mobileNavigation.getBoundingClientRect();
+  if (
+    e.clientX < dialogDimensions.left ||
+    e.clientX > dialogDimensions.right ||
+    e.clientY < dialogDimensions.top ||
+    e.clientY > dialogDimensions.bottom
+  ) {
+    closeMobileNavigation();
+  }
+}
+
+/* Cycle on focusable elements in the mobile navigation */
+function handleKeyDown(e) {
+  if (e.key === "Escape") {
+    closeMobileNavigation();
+  }
+
+  if (e.key !== "Tab") return;
+
+  const focusableElements = mobileNavigation.querySelectorAll(
+    'a[href], button:not([disabled]), [tabindex]:not([tabindex="-1"])'
+  );
+  const firstFocusable = focusableElements[0];
+  const lastFocusable = focusableElements[focusableElements.length - 1];
+
+  if (e.shiftKey) {
+    if (document.activeElement === firstFocusable) {
+      e.preventDefault();
+      lastFocusable.focus();
+    }
+  } else {
+    if (document.activeElement === lastFocusable) {
+      e.preventDefault();
+      firstFocusable.focus();
+    }
+  }
+}
+
+function openMobileNavigation() {
+  mobileNavigation.showModal();
+  body.classList.add("scroll-locked");
+  const firstFocusable = mobileNavigation.querySelector(
+    "a[href], button:not([disabled])"
+  );
+  if (firstFocusable) firstFocusable.focus();
+}
+
+function closeMobileNavigation() {
+  mobileNavigation.close();
+  body.classList.remove("scroll-locked");
+}
+
+function init() {
+  setupEventListeners();
+}
+
+document.addEventListener("DOMContentLoaded", init);
